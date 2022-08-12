875fc94141749f87cad0110e7c2c751974ad7236
gituser10003 <githan1203@gmail.com>
2022-08-12 16:08:48 +0900

    수정페이지

--- src/main/java/com/controller/Goods/ProductAddServlet.java
+++ src/main/java/com/controller/Goods/ProductAddServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import com.dto.ProductDTO;
+import com.dto.SawonDTO;
 import com.service.ProductService;
 
 /**
@@ -30,25 +31,37 @@ public class ProductAddServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
+		 HttpSession session = request.getSession();
+	      SawonDTO sdto = (SawonDTO)session.getAttribute("login");
+		 String nextPage = null;
+	      if(sdto!=null) {//회원인경우 
 		String pdno=request.getParameter("pdno");
 		String pdnm=request.getParameter("pdnm");
 		String pdprice=request.getParameter("pdprice");
 		String ctno=request.getParameter("ctno");
 		String pdfile=request.getParameter("pdfile");
 	
-		ProductDTO dto=new ProductDTO(pdno, pdnm, 0, ctno, pdfile);
+		ProductDTO dto=new ProductDTO();
+		dto.setPdno(pdno);
+		dto.setPdnm(pdnm);
+		dto.setPdprice(Integer.parseInt(pdprice));
+		dto.setCtno(ctno);
+		dto.setPdfile(pdfile);
 	
 		ProductService service=new ProductService();
 		int n=service.ProductAdd(dto);
 		
-		HttpSession session = request.getSession();
+		nextPage = "ProductRetrieveServlet?pdno="+pdno;
 		session.setAttribute("ProductAdd", "상품기입성공");
 		session.setMaxInactiveInterval(5);
-		response.sendRedirect("ProductListServlet");
-		
-		
-		
+		/* response.sendRedirect("ProductListServlet"); */
+
+      }else {
+		  nextPage = "LoginUIServlet";
+		  session.setAttribute("mesg", "로그인이 필요한 작업입니다.");
+	  }
+     
+    response.sendRedirect(nextPage);
 		
 	}
 
