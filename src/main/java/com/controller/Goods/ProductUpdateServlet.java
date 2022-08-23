d88b7dd2f8a6eb28dd1cf5f57d0fba79fbb2fdfc
gituser10003 <githan1203@gmail.com>
2022-08-23 16:09:06 +0900

    프로덕트 이미지 수정중

--- src/main/java/com/controller/Goods/ProductUpdateServlet.java
+++ src/main/java/com/controller/Goods/ProductUpdateServlet.java
@@ -22,18 +22,9 @@ import com.service.ProductService;
 @WebServlet("/ProductUpdateServlet")
 public class ProductUpdateServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ProductUpdateServlet() {
-        super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		SawonDTO dto = (SawonDTO)session.getAttribute("login");
